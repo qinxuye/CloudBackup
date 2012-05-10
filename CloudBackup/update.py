f61965e19fdfd97b4a5a657639429aed4cedfaa6
wang zheng windows <cswangzheng@gmail.com>
2012-05-10 20:20:34 +0800

    now can delete empty folder in the vdisk

--- CloudBackup/update.py
+++ CloudBackup/update.py
@@ -8,6 +8,7 @@ filefolder update
 
 from CloudBackup.cloud import VdiskStorage, S3Storage
 import os
+import time
 
 class hand(object):
     '''
@@ -18,7 +19,7 @@ class hand(object):
         '''
         Constructor
         '''
-    def  update(self,path,vdiskstorage):
+    def  update(self,path,storage):
         
         if os.path.isdir(path)==False:
             pass
@@ -26,45 +27,30 @@ class hand(object):
         firstpath=os.path.split(path)[0]
         lastpath=os.path.split(path)[1]
     
-        print firstpath,lastpath
+        print "Current File folder is",firstpath,lastpath
         
+        folderdict={}
+        for netfolder in storage.list(lastpath):
+            folderdict.update({netfolder.path:0})
         netdict={}
-        
-        for netfile in vdiskstorage.list_files(lastpath,True):
-                
+        for netfile in storage.list_files(lastpath,True): 
+                #print netfile.path
+                folderdict[netfile.path]=1          
                 if os.sep=='\\':
                     netfile.path=netfile.path.replace('/','\\')
-                netmodetime=netfile.path.split('_time_')[-1]
-                dlfile=firstpath+netfile.path.replace('_time_'+netmodetime,'')
+                netmodetime=netfile.path.split('.t')[-1]
+                dlfile=firstpath+netfile.path.replace('.t'+netmodetime,'')
                 netdict.update({dlfile:netmodetime})
-                
-        print netdict.keys(),netdict.values()
-        
-        localdict={}
-               
+
+        localdict={}                
         for dirpath, dirnames, filenames in os.walk(path):
             for filename in filenames:
                 filepath=os.path.join(dirpath,filename)
-                lastmtime=int(os.path.getmtime(filepath))
-                
-                print filepath,lastmtime
-                
+                lastmtime=int(os.path.getmtime(filepath))                
                 localdict.update({filepath:lastmtime})
-                
-                '''
-                
-                tuploadpath=filepath.replace(firstpath,'')+'_time_'+str(lastmtime)
-                if os.sep=='\\':
-                    tuploadpath=tuploadpath.replace('\\','/')
-                
-                vdiskstorage.upload(tuploadpath,filepath)
-                print tuploadpath
-        '''
         
         nethand={}#0:nothing 1:delete 2:download
         localhand={}#0:nothing 1:delete 2:upload
-        
-        print localdict.keys(),localdict.values()
         
         for netkey in netdict.keys():
             netvalue=int(round(float(netdict[netkey])))
@@ -75,47 +61,59 @@ class hand(object):
                 if  netvalue > localvalue:#net is newer than local
                     nethand.update({netkey:2})
                     localhand.update({netkey:1})
-                    print ">"
+                    #print ">"
                 if  netvalue < localvalue:#local is newer than net
                     nethand.update({netkey:1})
                     localhand.update({netkey:2})
-                    print "<"
+                    #print "<"
                 if  netvalue == localvalue:
                     nethand.update({netkey:0})
                     localhand.update({netkey:0})
-                    print "=="
+                    #print "=="
                 
                 
         for localkey in localdict.keys():
             if  localhand.has_key(localkey)==False:
                 localhand.update({localkey:2})
        
-        print nethand.keys(),nethand.values()
-        print localhand.keys(),localhand.values()
+        #print nethand.keys(),nethand.values()
+        #print localhand.keys(),localhand.values()
         
         for nethandkey in nethand.keys():
-            uploadpath=nethandkey.replace(firstpath,'')+'_time_'+netdict[nethandkey]
+            uploadpath=nethandkey.replace(firstpath,'')+'.t'+netdict[nethandkey]
             if os.sep=='\\':
                 uploadpath=uploadpath.replace('\\','/')
-            if nethand[nethandkey]==1:
-                vdiskstorage.delete(uploadpath)
+            if nethand[nethandkey]==1:    
+                storage.delete(uploadpath)
+                print "delete in Cloud",uploadpath
             if nethand[nethandkey]==2:
                 if localhand.has_key(nethandkey) and localhand[nethandkey]==1:
                     os.remove(nethandkey)
+                    print "delete",nethandkey
                 print nethandkey,uploadpath
-                vdiskstorage.download(uploadpath,nethandkey)
+                storage.download(uploadpath,nethandkey)
             
         for localhandkey in localhand.keys():
-            uploadpath=localhandkey.replace(firstpath,'')+'_time_'+str(localdict[localhandkey])
+            uploadpath=localhandkey.replace(firstpath,'')+'.t'+str(localdict[localhandkey])
             if os.sep=='\\':
                 uploadpath=uploadpath.replace('\\','/')
             if localhand[localhandkey]==2:
-                print uploadpath
-                vdiskstorage.upload(uploadpath,str(localhandkey))
-        '''
-        for name in vdiskstorage.list_files('',True):
-            vdiskstorage.delete(name.path)
-            print name.path
-        '''
+                print "upload",localhandkey,"to",uploadpath
+                storage.upload(uploadpath,str(localhandkey))
         
+        
+        '''
+        used to delete empty folder
+        '''
+        print "Folder"
+        for netfolder in folderdict.keys():
+            if folderdict[netfolder]==0:
+                print netfolder
+                print [tempfile for tempfile in storage.list(netfolder)].__len__()
+                if [tempfile for tempfile in storage.list(netfolder)].__len__()==0:
+                    
+                    storage.delete(netfolder)
+                    print "delete",netfolder
+
+        print "Update Complete in",time.ctime()
         
